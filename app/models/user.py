2ede42e706de28eeaa6cbae30f687ac54427564d
Bryan Thompson <bthompson1223@outlook.com>
2024-02-27 15:27:03 -0600

    updated user model

--- app/models/user.py
+++ app/models/user.py
@@ -11,8 +11,13 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    profile_img = db.Column(db.String)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
     @property
     def password(self):
@@ -29,5 +34,10 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'profile_img': self.profile_img,
+            'email': self.email,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
