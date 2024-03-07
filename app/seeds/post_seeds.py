b9e0ecd5befd2ef1db7d5694c34ba1de62762f64
Bryan Thompson <bthompson1223@outlook.com>
2024-03-07 10:36:11 -0600

    add thirty starter seed posts to be modified

--- app/seeds/post_seeds.py
+++ app/seeds/post_seeds.py
@@ -5,5 +5,208 @@ posts = [
        'body': "I'm just curious what everyone's favorite RPG is",
        'community_id': 1,
        'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
+    },
+    {
+       'owner_id': 1,
+       'title': "What's your favorite RPG?",
+       'body': "I'm just curious what everyone's favorite RPG is",
+       'community_id': 1,
+       'image_url': "https://vghaven-bucket.s3.us-east-2.amazonaws.com/1cr2fwoisveb1.jpg" 
     }
 ]
