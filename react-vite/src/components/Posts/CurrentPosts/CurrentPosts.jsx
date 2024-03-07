77935fe1bf4e1379da7f0dd45a593c136c315a4b
Bryan Thompson <bthompson1223@outlook.com>
2024-03-06 18:38:29 -0600

    npm run build

--- react-vite/src/components/Posts/CurrentPosts/CurrentPosts.jsx
+++ react-vite/src/components/Posts/CurrentPosts/CurrentPosts.jsx
@@ -5,6 +5,7 @@ import {
   thunkGetCurrentUserPosts,
 } from "../../../redux/post";
 import PostList from "../PostList/PostList";
+import "./CurrentPosts.css";
 
 const CurrentPosts = () => {
   const user = useSelector((state) => state.session.user);
@@ -21,12 +22,16 @@ const CurrentPosts = () => {
   if (!user) return <h1>You must be logged in to see your current posts!</h1>;
   if (!Object.values(postsObj).length) return <h1>No Posts Yet!</h1>;
 
-  const posts = Object.values(postsObj);
+  const posts = Object.values(postsObj).filter(
+    (post) => post.owner_id === user.id
+  );
 
   return (
-    <div className="user-posts-container">
-      <h2>{user.username}&apos;s Posts</h2>
-      <PostList passedInPosts={posts} />
+    <div className="user-post-root">
+      <div className="user-posts-container">
+        <h2>{user.username}&apos;s Posts</h2>
+        <PostList passedInPosts={posts} />
+      </div>
     </div>
   );
 };
